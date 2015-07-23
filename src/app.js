ea175ca64787d5e1963a879f4bd242cc8c397be5
Jason Benn <jasoncbenn@gmail.com>
2015-07-23 00:02:59 -0700

    Canvas renders a spiral.

--- src/app.js
+++ src/app.js
@@ -3,4 +3,10 @@ import React from 'react';
 import 'normalize.css'
 import './styles/main.scss'
 
-React.render(<Canvas />, document.getElementById('main'));
+React.render(<Canvas 
+  height={500}
+  width={500}
+  lineWidth={2}
+  gridSize={20}
+  color='rgb(200,100,150)'
+/>, document.getElementById('main'));
--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { getSpiralPoints } from '../get-spiral-points'
+import { List } from 'immutable'
 import '../styles/canvas.scss'
 
 export class Canvas extends React.Component {
+  componentDidMount() {
+    const $el = React.findDOMNode(this)
+    const ctx = $el.getContext('2d')
+
+    ctx.strokeStyle = this.props.color;
+    ctx.lineWidth = this.props.lineWidth
+
+    const paddingRatio = .1
+    const spiralHeight = this.props.height * (1 - paddingRatio * 2)
+    const spiralWidth = this.props.width * (1 - paddingRatio * 2)
+    const topLeftX = this.props.width * paddingRatio
+    const topLeftY = this.props.height * paddingRatio
+    const heightScaleFactor = spiralHeight / this.props.gridSize
+    const widthScaleFactor = spiralWidth / this.props.gridSize
+
+    ctx.beginPath()
+    ctx.moveTo(topLeftX, topLeftY)
+    const spiralPoints = getSpiralPoints(this.props.gridSize)
+    spiralPoints.forEach(([row, col]) => {
+      ctx.lineTo((col * widthScaleFactor) + topLeftX, (row * heightScaleFactor) + topLeftY)
+    })
+    ctx.stroke()
+  }
+
   render() {
     return (
-      <canvas></canvas>
+      <canvas height={this.props.height} width={this.props.width}></canvas>
     );
   }
 }
