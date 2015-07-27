0f7ddadd21cb7218d7e03d77b63899feab8f49cd
Jason Benn <jasoncbenn@gmail.com>
2015-07-27 09:08:36 -0700

    TextInput needs to serialize type=number fields to numbers (normally they're strings). Why can't it do that by itself, I wonder?

--- web/src/components/SpiralForm.js
+++ web/src/components/SpiralForm.js
@@ -3,13 +3,13 @@ import $ from 'jquery'
 import {TextInput} from './TextInput'
 
 export class SpiralForm extends React.Component {
-  static fields = ['email', 'thickness', 'gridSize', 'color']
+  static fields = [['email', 'text'], ['thickness', 'number'], ['gridSize', 'number'], ['color', 'text']]
 
   render() {
     return (
       <form action="/api/spirals" method="POST" encType="multipart/form-data" onSubmit={this.props.onSubmit}>
-        {this.constructor.fields.map((field, i) => {
-          return <TextInput onChange={this.props.onFieldChange} value={this.props[field]} name={field} key={i} />
+        {this.constructor.fields.map(([field, type], i) => {
+          return <TextInput type={type} onChange={this.props.onFieldChange} value={this.props[field]} name={field} key={i} />
         })}
         <input type="submit" value="Save" />
       </form>
--- web/src/components/TextInput.js
+++ web/src/components/TextInput.js
@@ -4,12 +4,21 @@ import { Map } from 'immutable'
 export class TextInput extends React.Component {
   onChange = (e) => {
     e.preventDefault()
-    this.props.onChange(Map([[this.props.name, e.target.value]]).toJS())
+    this.props.onChange(this.serialize(e.target.value))
+  }
+
+  serialize(rawValue) {
+    const value = this.props.type === 'number' ? +rawValue : rawValue
+    return Map([[this.props.name, value]]).toJS()
+  }
+
+  static defaultProps = {
+    type: 'text'
   }
 
   render() {
     return (
-      <input type='text' value={this.props.value} onChange={this.onChange} placeholder={this.props.name} />
+      <input type={this.props.type} value={this.props.value} onChange={this.onChange} placeholder={this.props.name} />
     )
   }
 
