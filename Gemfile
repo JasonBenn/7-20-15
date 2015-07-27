96fac17667a975135c051f0501e49c0610a2b37a
Jason Benn <jasoncbenn@gmail.com>
2015-07-27 01:46:45 -0700

    Simple backend tests! Unfortunately they're using the same database...

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem "sinatra-cross_origin"
 gem 'data_uri'
 gem 'pg'
 gem 'thor'
+gem 'rack-test'
---
+++ test/api-test.rb
@@ -0,0 +1,31 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../api'
+require 'test/unit'
+require 'rack/test'
+
+class APITest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_it_responds
+    get '/api/spirals'
+    assert last_response.ok?
+  end
+
+  def test_it_gets_a_spiral
+    get '/api/spirals/1'
+    assert_equal last_response.body, {
+        "modified_at": "2015-07-25 22:57:15.8726-07",
+        "id": "1",
+        "email": "jasoncbenn@gmail.com",
+        "thickness": "5",
+        "grid_size": "30",
+        "color": "green",
+        "image_url": nil
+      }.to_json
+  end
+end
